bf15e27ff222b093fa39013099f469c6db22905a
hun0219 <gsw98000@gmail.com>
2024-12-06 14:27:35 +0900

    url 캐시 초기화 수정

--- crawling/saramin/saramin_url_crawling.py
+++ crawling/saramin/saramin_url_crawling.py
@@ -50,8 +50,7 @@ chrome_options.add_argument("--incognito")
 chrome_options.add_argument("--disable-gpu")  # GPU 캐시 비활성화 (필요한 경우)
 chrome_options.add_argument("--no-sandbox")  # 샌드박스 비활성화 (권장)
 chrome_options.add_argument("--disable-dev-shm-usage")  # 공유 메모리 비활성화 (리소스 관리)
-#chrome_options.add_argument("--user-data-dir=/tmp/chrome-profile")
-#chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-features=NetworkService,NetworkServiceInProcess")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36")  # User-Agent 설정
 
