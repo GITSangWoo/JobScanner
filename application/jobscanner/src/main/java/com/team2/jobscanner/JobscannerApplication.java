696cdb6c018569d0523391cc7fde8f5eaa5a4810
GITSangWoo <dogcenta@gmail.com>
2024-12-11 16:30:38 +0900

    fixed

--- application/jobscanner/src/main/java/com/team2/jobscanner/JobscannerApplication.java
+++ application/jobscanner/src/main/java/com/team2/jobscanner/JobscannerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobscannerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JobscannerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(JobscannerApplication.class, args);
+	}
 
 }
