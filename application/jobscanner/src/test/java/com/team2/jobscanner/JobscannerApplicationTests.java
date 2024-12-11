477ef207cd1caf6b0614304cd4a6f055c265d8a6
GITSangWoo <dogcenta@gmail.com>
2024-12-11 15:50:08 +0900

    fixing

--- application/jobscanner/src/test/java/com/team2/jobscanner/JobscannerApplicationTests.java
+++ application/jobscanner/src/test/java/com/team2/jobscanner/JobscannerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JobscannerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
