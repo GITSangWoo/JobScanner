6b49a96648e9eb8da808a057ccafbe178b8f0826
GITSangWoo <dogcenta@gmail.com>
2024-12-12 18:56:05 +0900

    table making

---
+++ application/jobscanner/src/main/java/com/team2/jobscanner/Hello.java
@@ -0,0 +1,10 @@
+package com.team2.jobscanner;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Hello {
+    private String data;
+    
+}
--- application/jobscanner/src/main/java/com/team2/jobscanner/JobscannerApplication.java
+++ application/jobscanner/src/main/java/com/team2/jobscanner/JobscannerApplication.java
@@ -3,10 +3,11 @@ package com.team2.jobscanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+ @SpringBootApplication
 public class JobscannerApplication {
 
 	public static void main(String[] args) {
+	
 		SpringApplication.run(JobscannerApplication.class, args);
 	}
 
