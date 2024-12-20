507d70c0e08910ff525684c317fc064b92d8f665
DONGUK777 <kimdongyork@gmail.com>
2024-12-20 11:47:38 +0900

    some eidt

--- application/jobscanner/src/main/java/com/team2/jobscanner/dto/RankCategoryDTO.java
+++ application/jobscanner/src/main/java/com/team2/jobscanner/dto/RankCategoryDTO.java
@@ -6,12 +6,12 @@ import lombok.Getter;
 
 @Getter
 public class RankCategoryDTO {
-    private String jobTitle;  // 직무 이름
+    private String jobtitle;  // 직무 이름
     private String category;  // 카테고리 이름
     private List<RankDTO> ranks;  // 해당 카테고리의 기술 스택 순위 리스트
 
-    public RankCategoryDTO(String jobTitle, String category, List<RankDTO> ranks) {
-        this.jobTitle = jobTitle;
+    public RankCategoryDTO(String jobtitle, String category, List<RankDTO> ranks) {
+        this.jobtitle = jobtitle;
         this.category = category;
         this.ranks = ranks;
     }
