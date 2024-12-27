b43b29502c8b3a5e151c840d8aa42446f25e8a46
GITSangWoo <dogcenta@gmail.com>
2024-12-27 14:42:53 +0900

    finding problem

--- application/jobscanner/src/main/java/com/team2/jobscanner/controller/OAuthController.java
+++ application/jobscanner/src/main/java/com/team2/jobscanner/controller/OAuthController.java
@@ -18,7 +18,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-@CrossOrigin(origins = "http://43.202.114.11:8972")
+@CrossOrigin(origins = "http://43.202.114.11:8973")
 @RequestMapping("/auth")
 @RestController
 public class OAuthController {
