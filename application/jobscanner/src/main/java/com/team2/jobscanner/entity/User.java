a55b26f2e716e663adc1c52ffa0a9544079768f8
GITSangWoo <dogcenta@gmail.com>
2024-12-28 21:19:35 +0900

    build error fix

--- application/jobscanner/src/main/java/com/team2/jobscanner/entity/User.java
+++ application/jobscanner/src/main/java/com/team2/jobscanner/entity/User.java
@@ -4,8 +4,6 @@ import com.team2.jobscanner.time.AuditTime;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.security.web.server.authentication.ott.ServerOneTimeTokenAuthenticationConverter;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
