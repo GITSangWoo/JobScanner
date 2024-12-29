8c56c2dafe6fb76cc492ae38d3fe67c833a823cc
GITSangWoo <dogcenta@gmail.com>
2024-12-29 09:45:40 +0900

    rollback

--- application/jobscanner/src/main/java/com/team2/jobscanner/entity/Auth.java
+++ application/jobscanner/src/main/java/com/team2/jobscanner/entity/Auth.java
@@ -11,8 +11,10 @@ import java.time.LocalDateTime;
 @Getter
 @Entity
 public class Auth {
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
